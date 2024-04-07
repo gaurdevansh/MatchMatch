e0040500208e065f4a5afcb50fc35ee5a41bb6d8
Devansh <devanshgaur75@gmail.com>
2024-04-07 12:34:24 +0530

    Added level selection fragment, game fragment and navigation

--- app/src/main/java/com/example/matchmatch/MainActivity.kt
+++ app/src/main/java/com/example/matchmatch/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.matchmatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
     }
+
+    fun navigateToGameScreen(selectedLevel: Int) {
+
+    }
+
 }
